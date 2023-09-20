6dded207f2e4322434b1273cf2ab86fd4c3608ed
EMILIA INÉS ARMENO <47697893@ort>
2023-09-20 10:00:12 -0300

    listo

--- Models/Alumnos.cs
+++ Models/Alumnos.cs
@@ -5,14 +5,18 @@ public class Alumnos
    public string Legajo{get;set;}
    public string Curso{get;set;}
     public int IdNota{get;set;}
+    public int Promedio{get;set;}
+    public string Especialidad{get;set;}
 
     public Alumnos(){}
-   public Alumnos(string Nombre, string Apellido, string Legajo, string Curso, int IdNota){
-    string nombre=Nombre;
-    string apellido=Apellido;
-    string legajo=Legajo;
-    string curso=Curso; 
-    int idNota=IdNota; 
+   public Alumnos(string Nombre, string Apellido, string Legajo, string Curso, int IdNota, int Promedio, string Especialida){
+        string nombre=Nombre;
+        string apellido=Apellido;
+        string legajo=Legajo;
+        string curso=Curso; 
+        int idNota=IdNota; 
+        int promedio=Promedio;
+        string especialidad=Especialida;
    }
 
 }
